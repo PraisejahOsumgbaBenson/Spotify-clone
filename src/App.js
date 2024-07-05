1c19f90a6dca3ce7cd928a8d86b448c81088c6fb
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-07-05 12:13:54 +0100

    feat(auth): integrate state management for token handling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React,{useEffect} from "react";
 import Login from "./components/Login";
+import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/Constant";
+import Spotify from "./components/Spotify";
 
 export default function App(){
+  const [{token},dispatch] = useStateProvider();
   useEffect(()=> {
    const hash = window.location.hash;
    if(hash){
     const token = hash.substring(1).split("&")[0].split('=')[1];
-    console.log(token);
+    dispatch({type:reducerCases.SET_TOKEN, token})
    }
-  },[])
-  return(
-  <div>
-    <Login/>
-  </div>
-  )
+  },[token, dispatch])
+  return <div>{token ? <Spotify /> : <Login />}</div>;
+  
 }
