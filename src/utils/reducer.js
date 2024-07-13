d3898a77ada1bd3df79d0322f07ae25a3ce12a02
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-07-13 19:38:13 +0100

    feat(reducer): extend state management with additional cases

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -1,19 +1,49 @@
 import { reducerCases } from "./Constant";
 
+// Initial state with properties for token, playlists, userInfo, selectedPlayListId, and selectedPlayList
 export const initialSate = {
-    token: null,
+  token: null,
+  playlists: [], // Array to store playlists
+  userInfo: null, // Object to store user information
+  selectedPlayListId: "6VoB9eBTXefcST5sV6KtdH", // Default selected playlist ID
+  selectedPlayList: null, // Object to store selected playlist details
 };
 
-const reducer = (state,action) => {
-    switch(action.type){
-        case reducerCases.SET_TOKEN :{
-            return{
-                ...state,token:action.token,
-            }
-        }
-        default:
-            return state;
+// Reducer function to handle state transitions based on action types
+const reducer = (state, action) => {
+  switch (action.type) {
+    // Case to set the token in the state
+    case reducerCases.SET_TOKEN: {
+      return {
+        ...state,
+        token: action.token,
+      };
     }
+    // Case to set playlists in the state
+    case reducerCases.SET_PLAYLISTS: {
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    }
+    // Case to set user information in the state
+    case reducerCases.SET_USER: {
+      return {
+        ...state,
+        userInfo: action.userInfo,
+      };
+    }
+    // Case to set the selected playlist in the state
+    case reducerCases.SET_PLAYLIST: {
+      return {
+        ...state,
+        selectedPlayList: action.selectedPlayList,
+      };
+    }
+    // Default case returns the current state if no matching action type is found
+    default:
+      return state;
+  }
 };
 
 export default reducer;
