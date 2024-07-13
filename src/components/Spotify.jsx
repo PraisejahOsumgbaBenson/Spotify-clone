d3ae44fd55a00067f6a971f9b4a2aaf564bc459e
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-07-13 19:31:28 +0100

    feat: integrate user info fetching from Spotify API in Spotify component

--- src/components/Spotify.jsx
+++ src/components/Spotify.jsx
@@ -1,25 +1,55 @@
-import React from "react"
-import styled from "styled-components"
+import React, { useEffect } from "react";
+import styled from "styled-components";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
 import Body from "./Body";
 import Footer from "./Footer";
-export default function Spotify(){
-    return <Container>
-        <div className="spotify__body">
-            <Sidebar />
-            <div className="body">
-                <Navbar />
-               <div className="body__contents">
-                <Body />
-               </div>
-            </div>
+import { useStateProvider } from "../utils/StateProvider";
+import axios from "axios";
+import { reducerCases } from "../utils/Constant";
+
+// Spotify component fetches user information from Spotify API on mount
+// and dispatches it to the state provider for global state management
+export default function Spotify() {
+  const [{ token }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      // Fetch user information from Spotify API
+      const { data } = await axios.get("https://api.spotify.com/v1/me", {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      });
+
+      // Dispatch user information to the global state
+      const userInfo = {
+        userId: data.id,
+        userName: data.display_name,
+      };
+      dispatch({ type: reducerCases.SET_USER, userInfo });
+    };
+
+    getUserInfo();
+  }, [dispatch, token]);
+
+  return (
+    <Container>
+      <div className="spotify__body">
+        <Sidebar />
+        <div className="body">
+          <Navbar />
+          <div className="body__contents">
+            <Body />
+          </div>
         </div>
-        <div className="spotify__footer">
-            <Footer />
-        </div>
+      </div>
+      <div className="spotify__footer">
+        <Footer />
+      </div>
     </Container>
-    
+  );
 }
 
 const Container = styled.div`
@@ -39,6 +69,6 @@ const Container = styled.div`
   .body {
     height: 100%;
     width: 100%;
-    overflow:auto;
+    overflow: auto;
   }
 `;
