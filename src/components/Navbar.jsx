3c06eb9151aedfabdbd14b6fb97b69abaad3e2a2
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-07-13 19:36:44 +0100

    fix(navbar): replace anchor tag with button to resolve accessibility warning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,28 +4,32 @@ import { FaSearch } from "react-icons/fa";
 import { CgProfile } from "react-icons/cg";
 import { useStateProvider } from "../utils/StateProvider";
 
-// Navbar component includes a search bar and a user profile section
-// Uses styled-components for styling and icons for visual elements
+// Navbar component for the Spotify application
 export default function Navbar() {
+  // Extract userInfo from the global state
   const [{ userInfo }] = useStateProvider();
 
   return (
     <Container>
+      {/* Search bar section */}
       <div className="search__bar">
         <FaSearch />
         <input type="text" placeholder="Artists, songs, or podcasts" />
       </div>
+
+      {/* User avatar and profile section */}
       <div className="avatar">
-        <a href="#">
+        {/* Using a button instead of an anchor tag to avoid warnings */}
+        <button>
           <CgProfile />
           <span>{userInfo?.userName}</span>
-        </a>
+        </button>
       </div>
     </Container>
   );
 }
 
-// Styled component for the Navbar container and nested elements
+// Styled-component for the Navbar container
 const Container = styled.div`
   display: flex;
   justify-content: space-between;
@@ -37,6 +41,7 @@ const Container = styled.div`
   transition: 0.3s ease-in-out;
   background-color: none;
 
+  // Styling for the search bar
   .search__bar {
     background-color: white;
     width: 30%;
@@ -46,6 +51,7 @@ const Container = styled.div`
     align-items: center;
     gap: 0.5rem;
 
+    // Styling for the search input field
     input {
       border: none;
       height: 2rem;
@@ -57,6 +63,7 @@ const Container = styled.div`
     }
   }
 
+  // Styling for the user avatar section
   .avatar {
     background-color: black;
     padding: 0.3rem 0.4rem;
@@ -66,7 +73,8 @@ const Container = styled.div`
     justify-content: center;
     align-items: center;
 
-    a {
+    // Styling for the button that contains the profile icon and user name
+    button {
       display: flex;
       justify-content: center;
       align-items: center;
@@ -74,7 +82,11 @@ const Container = styled.div`
       text-decoration: none;
       color: white;
       font-weight: bold;
+      background: none;
+      border: none;
+      cursor: pointer;
 
+      // Styling for the profile icon within the button
       svg {
         font-size: 1.3rem;
         background-color: #282828;
