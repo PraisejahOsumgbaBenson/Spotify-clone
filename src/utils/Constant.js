4f43668268b31381ab9a3b53ec1638e7a40f39d6
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-08-08 18:04:46 +0100

    docs(reducerCases): add comments for clarity

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,8 +1,23 @@
+// Action types for the reducer to manage application state
 export const reducerCases = {
-    SET_TOKEN: "SET_TOKEN",
-    SET_PLAYLISTS: "SET_PLAYLISTS",
-    SET_USER: "SET_USER",
-    SET_PLAYLIST: "SET_PLAYLIST",
-    SET_PLAYING: "SET_PLAYING",
-    SET_PLAYER_STATE: "SET_PLAYER_STATE",
+  // Action type for setting the user's authentication token
+  SET_TOKEN: "SET_TOKEN",
+
+  // Action type for setting the user's playlists
+  SET_PLAYLISTS: "SET_PLAYLISTS",
+
+  // Action type for setting user information
+  SET_USER: "SET_USER",
+
+  // Action type for setting the currently selected playlist
+  SET_PLAYLIST: "SET_PLAYLIST",
+
+  // Action type for setting the currently playing track details
+  SET_PLAYING: "SET_PLAYING",
+
+  // Action type for setting the current state of the player (play/pause)
+  SET_PLAYER_STATE: "SET_PLAYER_STATE",
+
+  // Action type for setting the currently selected playlist ID
+  SET_PLAYLIST_ID: "SET_PLAYLIST_ID",
 };
