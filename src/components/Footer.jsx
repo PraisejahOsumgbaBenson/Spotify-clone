afe5029d17860c4b9f872f17eb9f7f915c0dda4d
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2024-08-08 17:48:29 +0100

    feat(Footer): add Volume component and update comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,24 +2,31 @@ import React from "react";
 import styled from "styled-components";
 import CurrentTrack from "./CurrentTrack";
 import PlayerControls from "./PlayerControls";
+import Volume from "./Volume";
+
+// Footer component renders the footer section of the application
 export default function Footer() {
   return (
-  <Container>
-    <CurrentTrack />
-    <PlayerControls/>
-  </Container>
+    <Container>
+      {/* Render the current track information */}
+      <CurrentTrack />
+      {/* Render player controls (play, pause, next, previous, etc.) */}
+      <PlayerControls />
+      {/* Render volume control */}
+      <Volume />
+    </Container>
   );
 }
 
+// Styled component for the footer container
 const Container = styled.div`
-  background-color: #181818;
-  height:100%;
-  width:100%;
-  border-top:1px solid #282828;
-  display:grid;
-  grid-template-columns:1fr 2fr 1fr;
-  align-items: center;
-  justify-content:center;
-  justify-content: center;
-  padding: 0 1rem;
+  background-color: #181818; 
+  height: 100%; 
+  width: 100%; 
+  border-top: 1px solid #282828; 
+  display: grid; 
+  grid-template-columns: 1fr 2fr 1fr; 
+  align-items: center; 
+  justify-content: center; 
+  padding: 0 1rem; 
 `;
